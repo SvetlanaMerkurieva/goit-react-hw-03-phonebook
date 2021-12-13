d54ac4b79be26fa5ae925d01562e0e78df9f54e1
SvetlanaMerkurieva <82316953+SvetlanaMerkurieva@users.noreply.github.com>
2021-12-13 11:08:28 +0200

    Adds localStorage

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -10,6 +10,18 @@ class App extends Component {
     contacts: [],
     filter: '',
   };
+  componentDidUpdate(prevProps, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
+      localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
+    }
+  }
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    const parsedContacts = JSON.parse(contacts);
+    if (parsedContacts) {
+      this.setState({ contacts: parsedContacts });
+    }
+  }
   handleFormSubmit = data => {
     const contact = {
       id: shortid.generate(),
